92e802d3a993f99e3bd90797e10d09b445062ad3
aliterekmani6@gmail.com <aliterekmani6@gmail.com>
2024-03-29 18:23:54 +0200

    BackEnd completed

--- server/Routes/BookRouter.js
+++ server/Routes/BookRouter.js
@@ -1,43 +1,108 @@
-import  express  from "express";
-import { BooksModel } from "../model/BooksModel.js";
+import express from 'express';
+import { BooksModel } from '../model/BooksModel.js';
+
 const router = express.Router();
 
-router.get("/" , async (req,res)=>{
-    try{
-        await BooksModel.find( {}, function(err, result ){ 
-            if (err || !result ) console.log(" an error has occurred" );
-            else {
-            console.log(result);
-            }
-            });
-    }catch(err){
-        console.log(err.message);
-        res.status(404).send({message : err.message})
+// Route for Save a new Book
+router.post('/', async (request, response) => {
+  try {
+    if (
+      !request.body.bookname ||
+      !request.body.author ||
+      !request.body.publishyear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      bookname: request.body.bookname,
+      author: request.body.author,
+      publishyear: request.body.publishyear,
+    };
+
+    const book = await BooksModel.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Books from database
+router.get('/', async (request, response) => {
+  try {
+    const books = await BooksModel.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get One Book from database by id
+router.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await BooksModel.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Update a Book
+router.put('/:id', async (request, response) => {
+  try {
+    if (
+      !request.body.bookname ||
+      !request.body.author ||
+      !request.body.publishyear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
     }
 
-})
+    const { id } = request.params;
 
+    const result = await BooksModel.findByIdAndUpdate(id, request.body);
 
-router.post("/addBook",async (req,res)=>{
-    try{
-        if(
-            !req.body.bookname ||
-            !req.body.author || 
-            !req.body.publishyear
-        ){
-            res.status(500).send("please send : book name , author and the publish year")
-        }else{
-            const newbook = req.body
-            const book =  BooksModel(newbook);
-            await book.save()
-            return res.status(200).send(book)
-        }
-
-    }catch(err){
-        console.log(err.message);
-        res.status(500).send(err.message)
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
     }
 
-})
+    return response.status(200).send({ message: 'Book updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Delete a book
+router.delete('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await BooksModel.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
+    }
+
+    return response.status(200).send({ message: 'Book deleted successfully' });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 export default router;
--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
 export const PORT = 3001;
-export const MongoDbUrl = "mongodb+srv://aliterekmani6:Ali123@cluster0.x6s681c.mongodb.net/Books";
+export const MongoDbUrl = "mongodb+srv://aliterekmani6:Ali123@cluster0.x6s681c.mongodb.net/Project";
